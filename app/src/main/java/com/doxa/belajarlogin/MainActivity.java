fd8f30d2df72c32d873a639302477658c56b03e6
Christian Doxa Hamasiah <zhergiuz@gmail.com>
2018-05-02 00:12:37 +0700

    - Initial commit

---
+++ app/src/main/java/com/doxa/belajarlogin/MainActivity.java
@@ -0,0 +1,102 @@
+package com.doxa.belajarlogin;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+public class MainActivity extends AppCompatActivity {
+    private static final int RC_SIGN_IN = 236;
+    private static final String TAG = MainActivity.class.getName();
+    GoogleSignInClient mGoogleSignInClient;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken("121374977809-iooq45qretjn7eefmkg99ivflh9hdg4q.apps.googleusercontent.com")
+                .requestEmail()
+                .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        SignInButton signInButton = findViewById(R.id.sign_in_button);
+        signInButton.setSize(SignInButton.SIZE_STANDARD);
+
+        findViewById(R.id.sign_in_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signIn();
+            }
+        });
+    }
+
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+
+            // Signed in successfully, show authenticated UI.
+
+            if (account != null) {
+
+                String email = account.getEmail();
+
+                if (email.matches("^[a-zA-Z]{3,}@smktelkom-mlg\\.sch\\.id")) {
+                    Toast.makeText(this, "Masuk sebagai guru : " + account.getEmail(), Toast.LENGTH_SHORT).show();
+                    signOut();
+                } else if (email.matches("@student\\.smktelkom-mlg\\.sch\\.id$")) {
+                    Toast.makeText(this, "Masuk sebagai siswa : " + account.getEmail(), Toast.LENGTH_SHORT).show();
+                    signOut();
+                } else {
+                    Toast.makeText(this, "Bukan email sekolah : " + account.getEmail(), Toast.LENGTH_SHORT).show();
+                    signOut();
+                }
+            }
+
+
+        } catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.w(TAG, "signInResult:failed code=" + e.getStatusCode());
+        }
+    }
+
+    private void signOut() {
+        mGoogleSignInClient.signOut()
+                .addOnCompleteListener(this, new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        Toast.makeText(MainActivity.this, "Otomatis Logout Success!", Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+}
